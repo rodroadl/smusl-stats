c0760506eaa1c5a9b9bd78a69bfb74716e9fd0e7
James Kim <kkgg3781@gmail.com>
2024-07-12 13:10:47 -0400

    update: rank

--- docs/script/script_index.js
+++ docs/script/script_index.js
@@ -3,8 +3,12 @@ $(document).ready(function(){
         var img = document.getElementById("elo-rating");
         var team = e.target.value;
         img.src = `fig/elo-ratings-bar-chart-${team}.png`;
-        var scrollToY = 2200 - parseInt(document.getElementById(team).innerText);
-        window.scrollTo(0,scrollToY);
+        if (team === "all"){
+            window.scrollTo(0,0);
+        }else{
+            var scrollToY = 2200 - parseInt(document.getElementById(team).innerText);
+            window.scrollTo(0,scrollToY);
+        }
     });
 
     $(".to-the-top").on("click", function(e) {
